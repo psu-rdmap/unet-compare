82e17bd3773d1a87c7635d5151263e8359e37914
suksia <aio5165@psu.edu>
2024-08-28 23:08:58 -0400

    refactored models.py and blocks.py; created an input dictionary python script

--- src/blocks.py
+++ src/blocks.py
@@ -1,56 +1,28 @@
-import keras
-from keras import layers
-import tensorflow as tf
-from tensorflow.keras.layers import *
+from tensorflow.keras.layers import Conv2D, BatchNormalization, Activation, Conv2DTranspose
 from keras.regularizers import l2
 
-# single Conv2D layer with
-#     batchnorm (and no bias if used)
-#     L2 regularization
-#     ReLU activation
-#     He normal initialization
-#     name indices
-def ConvRelu(input, filter, kernel_size, l2_reg, name_index, use_batchnorm=False):
-    x = Conv2D(filter, kernel_size, padding="same", name='conv'+name_index, use_bias=not(use_batchnorm), kernel_initializer='he_normal', kernel_regularizer=l2(l2_reg))(input)
-    if use_batchnorm:
-        x = BatchNormalization(name='bn'+name_index)(x)
-    x = Activation('relu', name='relu'+name_index)(x)
-    return x
-
-# single Conv2DTranspose layer with
-#     batchnorm (and no bias if used)
-#     L2 regularization
-#     ReLU activation
-#     He normal initialization
-#     name
-def ConvReluDecoder(input, filter, kernel_size, l2_reg, name_index, use_batchnorm=False):
-    x = Conv2DTranspose(filter, kernel_size, strides=(2,2), padding="same", name='up'+name_index, use_bias=not(use_batchnorm), kernel_initializer='he_normal', kernel_regularizer=l2(l2_reg))(input)
-    if use_batchnorm:
-        x = BatchNormalization(name='bn'+name_index)(x)
-    x = Activation('relu', name='relu'+name_index)(x)
-    return x
-
-# define standard convolution block (2 conv blocks)
-#     input tensor
-#     L2 regularization parameter
-#     stage in model for naming
-#     filters
-#     kernel size
-#     option to use batchnorm
-def encoder_unit(input, stage, filter, l2_reg, kernel_size=3, batchnorm=False):
-    x = ConvRelu(input, filter, kernel_size, l2_reg, name_index=stage+'_1', use_batchnorm=batchnorm)
-    x = ConvRelu(x,            filter, kernel_size, l2_reg, name_index=stage+'_2', use_batchnorm=batchnorm)
-    return x
-
-# define standard up convolution (decoder) block
-def decoder_unit(input, name_index, filter, l2_reg, kernel_size=2, batchnorm=False, decoder_type=1):
-    if decoder_type: # Conv2DTranspose
-        x = ConvReluDecoder(input, filter, kernel_size, l2_reg, name_index, use_batchnorm=batchnorm)
-
-    else: # UpSample -> Conv2D
-        x = Conv2D(filter, kernel_size, name='up'+name_index, padding='same', kernel_initializer='he_normal', use_bias=not(batchnorm), kernel_regularizer=l2(l2_reg))(UpSampling2D(size=(2, 2), name='upsample'+name_index)(input))
+def conv_block(input_tensor, index, num_filters, kernel_size=3, batchnorm=True, l2_reg=1e-3):
+    def conv_unit(input, unit_index):
+        conv = Conv2D(num_filters, kernel_size, padding='same', name='conv_'+unit_index, use_bias=not(batchnorm), kernel_initializer='he_normal', kernel_regularizer=l2(l2_reg))(input)
         if batchnorm:
-            x = BatchNormalization(name='bn_up'+name_index)(x)
-        x = Activation('relu', name='relu_up'+name_index)(x)
+            bn = BatchNormalization(name='bn_'+unit_index)(conv)
+            act = Activation('relu', name='relu_'+unit_index)(bn)
+        else:
+            act = Activation('relu', name='relu_'+unit_index)(conv)
 
-    return x
+        return act
+    
+    conv1 = conv_unit(input_tensor, index + 'a')
+    return conv_unit(conv1, index + 'b')
+
+
+
+def upsample_unit(input_tensor, index, num_filters, kernel_size=2, batchnorm=True, l2_reg=1e-3):
+    conv_up = Conv2DTranspose(num_filters, kernel_size, strides=(2,2), padding='same', name='up_'+index, use_bias=not(batchnorm), kernel_initializer='he_normal', kernel_regularizer=l2(l2_reg))(input_tensor)
+    if batchnorm:
+        bn = BatchNormalization(name='bn'+index)(conv_up)
+        act = Activation('relu', name='relu'+index)(bn)
+    else:
+        act = Activation('relu', name='relu'+index)(conv_up)
+    
+    return act
