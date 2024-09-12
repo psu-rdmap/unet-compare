e141fbec2b6f5509e27f5de6ccb553a1a5bfe270
suksia <aio5165@psu.edu>
2024-09-11 22:12:10 -0400

    added models docstring and place for Unet++

--- src/models.py
+++ src/models.py
@@ -1,3 +1,7 @@
+"""
+This module handles the definition of encoder/decoder subnetworks and their connection
+"""
+
 import keras
 import tensorflow as tf
 from tensorflow.keras.layers import MaxPooling2D, Conv2D, Concatenate
@@ -7,9 +11,10 @@ from tensorflow.keras.applications import EfficientNetB7
 
 
 class Encoder(tf.keras.Model):
-    def __init__(self, configs):
+    def __init__(self, configs : dict):
         super().__init__()
         
+        # unpack configs and assign them as instance attributes
         for key, value in configs.items():
             setattr(self, key, value)
 
@@ -50,38 +55,48 @@ class Encoder(tf.keras.Model):
     
 
 class Decoder(tf.keras.Model):
-    def __init__(self, configs):
+    def __init__(self, configs : dict):
         super().__init__()
         
+        # unpack configs and assign them as instance attributes
         for key, value in configs.items():
             setattr(self, key, value)
 
+        # instantiate encoder
         self.encoder = Encoder(configs).encoder
+
         self.build()
 
     def build(self):
         encoder_outputs = [self.encoder.get_layer(stage).output for stage in self.encoder_stages]
     
-        up3_1 = UpsampleUnit(self.decoder_filters[0], self.batchnorm, self.l2_reg, name='upsample_31')(encoder_outputs[4])
-        cat3_1 = Concatenate(name='cat_31')([up3_1, encoder_outputs[3]])
-        conv3_1 = ConvBlock(self.decoder_filters[0], batchnorm=self.batchnorm, l2_reg=self.l2_reg, name='conv_block_31')(cat3_1)
-        
-        up2_2 = UpsampleUnit(self.decoder_filters[1], self.batchnorm, self.l2_reg, name='upsample_22')(conv3_1)
-        cat2_2 = Concatenate(name='cat_22')([up2_2, encoder_outputs[2]])
-        conv2_2 = ConvBlock(self.decoder_filters[1], batchnorm=self.batchnorm, l2_reg=self.l2_reg, name='conv_block_22')(cat2_2)
-
-        up1_3 = UpsampleUnit(self.decoder_filters[2], self.batchnorm, self.l2_reg, name='upsample_13')(conv2_2)
-        cat1_3 = Concatenate(name='cat_13')([up1_3, encoder_outputs[1]])
-        conv1_3 = ConvBlock(self.decoder_filters[2], batchnorm=self.batchnorm, l2_reg=self.l2_reg, name='conv_block_13')(cat1_3)
-
-        up0_4 = UpsampleUnit(self.decoder_filters[3], self.batchnorm, self.l2_reg, name='upsample_04')(conv1_3)
-        cat0_4 = Concatenate(name='cat_04')([up0_4, encoder_outputs[0]])
-        conv0_4 = ConvBlock(self.decoder_filters[3], batchnorm=self.batchnorm, l2_reg=self.l2_reg, name='conv_block_04')(cat0_4)
+        if self.decoder_name == 'UNet':
+            up3_1 = UpsampleUnit(self.decoder_filters[0], self.batchnorm, self.l2_reg, name='upsample_31')(encoder_outputs[4])
+            cat3_1 = Concatenate(name='cat_31')([up3_1, encoder_outputs[3]])
+            conv3_1 = ConvBlock(self.decoder_filters[0], batchnorm=self.batchnorm, l2_reg=self.l2_reg, name='conv_block_31')(cat3_1)
             
-        output = Conv2D(1, 1, activation='sigmoid', name='output', kernel_initializer='he_normal',
-                                    padding='same', kernel_regularizer=l2(self.l2_reg))(conv0_4)
+            up2_2 = UpsampleUnit(self.decoder_filters[1], self.batchnorm, self.l2_reg, name='upsample_22')(conv3_1)
+            cat2_2 = Concatenate(name='cat_22')([up2_2, encoder_outputs[2]])
+            conv2_2 = ConvBlock(self.decoder_filters[1], batchnorm=self.batchnorm, l2_reg=self.l2_reg, name='conv_block_22')(cat2_2)
 
-        self.decoder = keras.Model(inputs=self.encoder.input, outputs=output, name='UNet')
+            up1_3 = UpsampleUnit(self.decoder_filters[2], self.batchnorm, self.l2_reg, name='upsample_13')(conv2_2)
+            cat1_3 = Concatenate(name='cat_13')([up1_3, encoder_outputs[1]])
+            conv1_3 = ConvBlock(self.decoder_filters[2], batchnorm=self.batchnorm, l2_reg=self.l2_reg, name='conv_block_13')(cat1_3)
+
+            up0_4 = UpsampleUnit(self.decoder_filters[3], self.batchnorm, self.l2_reg, name='upsample_04')(conv1_3)
+            cat0_4 = Concatenate(name='cat_04')([up0_4, encoder_outputs[0]])
+            conv0_4 = ConvBlock(self.decoder_filters[3], batchnorm=self.batchnorm, l2_reg=self.l2_reg, name='conv_block_04')(cat0_4)
+                
+            output = Conv2D(1, 1, activation='sigmoid', name='output', kernel_initializer='he_normal',
+                                        padding='same', kernel_regularizer=l2(self.l2_reg))(conv0_4)
+
+            self.decoder = keras.Model(inputs=self.encoder.input, outputs=output, name='UNet')
+
+        elif self.decoder_name == 'UNet++':
+            raise NotImplementedError('UNet++ not implemented yet')
+        
+        else:
+            raise ValueError('{} is not a valid decoder name'.format(self.decoder_name))
     
     def call(self):
         return self.decoder()
