fe03db5b5cc94b9fc6ab4b18deb43fc7aff4cfd9
suksia <aio5165@psu.edu>
2025-06-12 17:04:00 -0400

    Removing Operations class due to model references and GPU memory issues

--- src/run.py
+++ src/run.py
@@ -8,7 +8,7 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 os.environ['PYCARET_CUSTOM_LOGGING_LEVEL'] = 'CRITICAL'
 os.environ["CUDA_VISIBLE_DEVICES"] = '0'
-import argparse, json, shutil, gc
+import argparse, json, shutil, gc, keras
 import tensorflow as tf
 from keras.api.optimizers import Adam
 from keras.api.callbacks import CSVLogger, ModelCheckpoint, EarlyStopping
@@ -29,159 +29,152 @@ parser.add_argument('configs', type=str, help='Path to input configs file')
 args = parser.parse_args()
 
 
-class Operations:
-    """Singleton class for training and inference"""
+def single_loop(configs: dict):
+    """Trains a single model using a training and validation set"""
 
-    def __init__(self, configs: dict):
-        """Initialize configs, dataset, and model"""
-        self.configs = configs
-        self.dataset = None
-        self.model = None
+    # load dataset
+    print(f"\nCreating dataset from `{configs['dataset_name']}`...\n")
+    dataset = dataloader.create_train_dataset(configs)
+    print(f"Training images: {os_sorted(configs['training_set'])}")
+    print(f"Validation images: {os_sorted(configs['validation_set'])}\n")
 
-    def single_loop(self):
-        """Trains a single model using a training and validation set"""
+    # load model
+    print(f"Loading and compiling `{configs['encoder_name']}-{configs['decoder_name']}`...\n")
+    model = models.load_UNet(configs)
+    model.compile(
+        optimizer = Adam(learning_rate=configs['learning_rate']), 
+        loss = 'binary_crossentropy', 
+        metrics = ['accuracy', 'Precision', 'Recall']
+    )
 
-        # load dataset and model
-        print(f"\nCreating dataset from `{self.configs['dataset_name']}`...\n")
-        self.dataset = dataloader.create_train_dataset(self.configs)
-        print(f"Training images: {os_sorted(self.configs['training_set'])}")
-        print(f"Validation images: {os_sorted(self.configs['validation_set'])}\n")
-        print(f"Loading and compiling `{self.configs['encoder_name']}-{self.configs['decoder_name']}`...\n")
-        self.model = models.load_UNet(self.configs)
-        self.model.compile(
-            optimizer = Adam(learning_rate=self.configs['learning_rate']), 
-            loss = 'binary_crossentropy', 
-            metrics = ['accuracy', 'Precision', 'Recall']
+    # save model summary if desired
+    if configs['model_summary']:
+        utils.save_model_summary(configs, model)
+
+    # define training callbacks
+    callbacks = [
+        CSVLogger(
+            str(configs['results_dir'] / 'metrics.csv'), 
+            separator=',', 
+            append=False
+        ),
+        ModelCheckpoint(
+            str(configs['results_dir'] / 'best_model.keras'), 
+            verbose=1, 
+            save_best_only=True, 
+            save_weights_only=False
         )
+    ]
+    if configs['early_stopping']:
+        callbacks.append(EarlyStopping(patience = configs['patience']))
+    
+    # start training
+    print("Training model...\n")
+    history = model.fit(
+        dataset['train_dataset'],
+        epochs = configs['num_epochs'],
+        steps_per_epoch = dataset['train_steps'],
+        validation_data = dataset['val_dataset'],
+        validation_steps = dataset['val_steps'],
+        callbacks=callbacks,
+        verbose=2
+    )
 
-        # save model summary if desired
-        if self.configs['model_summary']:
-            utils.save_model_summary(self.configs, self.model)
+    # load best model and inference train/val sets
+    print("\nInferencing training and validation images with best model...\n")
+    inf_model = load_model(str(configs['results_dir'] / 'best_model.keras'))
+    inf_dataset = dataloader.create_train_val_inference_dataset(configs)
+    inference(configs, inf_dataset, inf_model)
 
-        # define training callbacks
-        callbacks = [
-            CSVLogger(
-                str(self.configs['results_dir'] / 'metrics.csv'), 
-                separator=',', 
-                append=False
-            ),
-            ModelCheckpoint(
-                str(self.configs['results_dir'] / 'best_model.keras'), 
-                verbose=1, 
-                save_best_only=True, 
-                save_weights_only=False
-            )
-        ]
-        if self.configs['early_stopping']:
-            callbacks.append(EarlyStopping(patience = self.configs['patience']))
-        
-        # start training
-        print("Training model...\n")
-        history = self.model.fit(
-            self.dataset['train_dataset'],
-            epochs = self.configs['num_epochs'],
-            steps_per_epoch = self.dataset['train_steps'],
-            validation_data = self.dataset['val_dataset'],
-            validation_steps = self.dataset['val_steps'],
-            callbacks=callbacks,
-            verbose=2
-        )
+    # plot metrics
+    print("\nPlotting metrics...\n")
+    utils.plot_results(configs)
 
-        # load best model and inference train/val sets
-        print("\nInferencing training and validation images with best model...\n")
-        self.model = load_model(str(self.configs['results_dir'] / 'best_model.keras'))
-        self.dataset = dataloader.create_train_val_inference_dataset(self.configs)
-        self.inference()
-
-        # plot metrics
-        print("\nPlotting metrics...\n")
-        utils.plot_results(self.configs)
-
-        print("Cleaning up...\n")
-        # remove training dataset and clear memory
-        shutil.rmtree(self.configs['root_dir'] / 'dataset')
-        del self.model, self.dataset, history, callbacks
-        K.clear_session()
-        gc.collect()
+    print("Cleaning up...\n")
+    # remove training dataset and clear memory
+    shutil.rmtree(configs['root_dir'] / 'dataset')
+    del model, inf_model, dataset, inf_dataset, history, callbacks
+    K.clear_session()
+    gc.collect()
 
 
-    def crossval_loop(self):
-        """Trains num_folds models using different non-overlapping validation sets"""
+def crossval_loop(configs: dict):
+    """Trains num_folds models using different non-overlapping validation sets"""
 
-        # determine all training and val set combinations given the number of folds
-        train_sets, val_sets = utils.create_folds(self.configs['training_set'], self.configs['num_folds'])
-        
-        # save original results directory
-        top_level_results = self.configs['results_dir']
-        
-        print(f"\nStarting cross validation with {self.configs['num_folds']} folds...\n")
+    # determine all training and val set combinations given the number of folds
+    train_sets, val_sets = utils.create_folds(configs['training_set'], configs['num_folds'])
+    
+    # save original results directory
+    top_level_results = configs['results_dir']
+    
+    print(f"\nStarting cross validation with {configs['num_folds']} folds...\n")
 
-        # loop through folds
-        for fold in range(self.configs['num_folds']):
-            print('-'*62 + ' Fold {} '.format(fold+1) + '-'*62)
+    # loop through folds
+    for fold in range(configs['num_folds']):
+        print('-'*62 + ' Fold {} '.format(fold+1) + '-'*62)
 
-            # update train/val sets
-            self.configs.update({'training_set' : train_sets[fold]})
-            self.configs.update({'validation_set' : val_sets[fold]})
+        # update train/val sets
+        configs.update({'training_set' : train_sets[fold]})
+        configs.update({'validation_set' : val_sets[fold]})
 
-            # create results directory for this fold
-            results_dir = self.configs['results_dir'] / ('fold_' + str(fold+1))
-            self.configs.update({'results_dir' : results_dir})
-            results_dir.mkdir()
+        # create results directory for this fold
+        results_dir = configs['results_dir'] / ('fold_' + str(fold+1))
+        configs.update({'results_dir' : results_dir})
+        results_dir.mkdir()
 
-            # train fold
-            self.single_loop()
+        # train fold
+        single_loop(configs)
 
-            # reset results directory for next fold
-            self.configs.update({'results_dir' : top_level_results})
+        # reset results directory for next fold
+        configs.update({'results_dir' : top_level_results})
 
-        # plot metrics over all folds
-        print("Plotting cross validation results...\n")
-        utils.cv_plot_results(self.configs)
+    # plot metrics over all folds
+    print("Plotting cross validation results...\n")
+    utils.cv_plot_results(configs)
 
 
-    def inference(self):
-        """Either inferences a training-validation pair of images, or just a single set of images"""
+def inference(configs: dict, dataset: dict, model: keras.Model):
+    """Either inferences a training-validation pair of images, or just a single set of images"""
 
-        if self.configs['operation_mode'] == 'train':
-            # process train and val images
-            train_preds = self.model.predict(self.dataset['train_dataset'], verbose=2)
-            val_preds = self.model.predict(self.dataset['val_dataset'], verbose=2)
+    if configs['operation_mode'] == 'train':
+        # process train and val images
+        train_preds = model.predict(dataset['train_dataset'], verbose=2)
+        val_preds = model.predict(dataset['val_dataset'], verbose=2)
 
-            # define and make output directories
-            train_save_dir = self.configs['results_dir'] / 'train_preds'
-            val_save_dir = self.configs['results_dir'] / 'val_preds'
-            train_save_dir.mkdir()
-            val_save_dir.mkdir()
+        # define and make output directories
+        train_save_dir = configs['results_dir'] / 'train_preds'
+        val_save_dir = configs['results_dir'] / 'val_preds'
+        train_save_dir.mkdir()
+        val_save_dir.mkdir()
 
-            # define pred save file paths
-            train_save_paths = [train_save_dir / (file.stem + '.png') for file in self.dataset['train_paths']]
-            val_save_paths = [val_save_dir / (file.stem + '.png') for file in self.dataset['val_paths']]
+        # define pred save file paths
+        train_save_paths = [train_save_dir / (file.stem + '.png') for file in dataset['train_paths']]
+        val_save_paths = [val_save_dir / (file.stem + '.png') for file in dataset['val_paths']]
 
-            # save predictions
-            utils.save_preds(train_preds, train_save_paths)
-            utils.save_preds(val_preds, val_save_paths)
+        # save predictions
+        utils.save_preds(train_preds, train_save_paths)
+        utils.save_preds(val_preds, val_save_paths)
 
-            del train_preds, val_preds, train_save_paths, val_save_paths
+        del model, dataset, train_preds, val_preds, train_save_paths, val_save_paths
 
-        else:
-            # load model and dataset
-            print("\nLoading data and model...\n")
-            self.model = load_model(str(self.configs['root_dir'] / self.configs['model_path']))
-            self.dataset = dataloader.create_inference_dataset(self.configs)
+    else:
+        # load model and dataset
+        print("\nLoading data and model...\n")
+        model = load_model(str(configs['root_dir'] / configs['model_path']))
+        dataset = dataloader.create_inference_dataset(configs)
 
-            # process dataset
-            print("Generating model predictions...\n")
-            preds = self.model.predict(self.dataset['dataset'], verbose=2)
+        # process dataset
+        print("Generating model predictions...\n")
+        preds = model.predict(dataset['dataset'], verbose=2)
 
-            # define output directories and paths
-            print("\nSaving model predictions...\n")
-            save_dir = self.configs['results_dir'] / 'preds'
-            save_dir.mkdir()
-            save_paths = [save_dir / (file.stem + '.png') for file in self.dataset['data_paths']]
+        # define output directories and paths
+        print("\nSaving model predictions...\n")
+        save_dir = configs['results_dir'] / 'preds'
+        save_dir.mkdir()
+        save_paths = [save_dir / (file.stem + '.png') for file in dataset['data_paths']]
 
-            # save predictions
-            utils.save_preds(preds, save_paths)
+        # save predictions
+        utils.save_preds(preds, save_paths)
 
 
 def main():
@@ -197,17 +190,14 @@ def main():
     configs = input_validator.validate(configs)
     utils.print_save_configs(configs.copy())
 
-    # start operations
-    operations = Operations(configs)
-
     # training or inference
     if configs['operation_mode'] == 'train':
         if configs['cross_validation']:
-            operations.crossval_loop()
+            crossval_loop()
         else:
-            operations.single_loop()
+            single_loop()
     else:
-        operations.inference()
+        inference()
     
     print("Done.")
 
